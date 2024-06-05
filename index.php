53e543c597442c6e5d5ec67cb6d2faabe8d80743
karolliszniewski <105976690+karolliszniewski@users.noreply.github.com>
2024-06-05 16:32:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="<?= get_template_directory_uri() ?>\dist\index.css">
+<script src="<?= get_template_directory_uri() ?>\dist\index.js?<?=filemtime(get_template_directory()."\dist\index.js") ?>"></script>
+<div class="w-10 h-16 bg-blue-300"></div>
+<?php
+
+echo "hello2";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-red"></div>
+</body>
+</html>
